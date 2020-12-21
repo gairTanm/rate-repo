66ccc93070388b85827468640afbf86986291627
Tanmay Gairola <tanmaygairola12@gmail.com>
2020-12-21 10:44:47 +0530

    validation and styling

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -4,16 +4,23 @@ import {
   View,
   TextInput,
   KeyboardAvoidingView,
+  Text,
 } from 'react-native';
 import { Formik, useField } from 'formik';
 import { Button } from 'react-native-paper';
+import * as yup from 'yup';
+
+const validationSchema = yup.object().shape({
+  username: yup.string().required('Username is required'),
+  password: yup.string().required('Password is required'),
+});
 
 const initialValues = {
   username: '',
   password: '',
 };
 
-const SignInForm = ({ onSubmit }) => {
+const SignInForm = ({ onSubmit, errors }) => {
   const [usernameField, usernameMeta, usernameHelpers] = useField('username');
   const [passwordField, passwordMeta, passwordHelpers] = useField('password');
   return (
@@ -21,19 +28,25 @@ const SignInForm = ({ onSubmit }) => {
       <TextInput
         label="Username"
         mode="outlined"
-        style={styles.input}
+        style={errors.username ? styles.error : styles.input}
         value={usernameField.value}
         onChangeText={text => usernameHelpers.setValue(text)}
         placeholder="Username"
       />
+      {errors.username ? (
+        <Text style={styles.errorText}>{errors.username}</Text>
+      ) : null}
       <TextInput
         label="Password"
         mode="outlined"
         placeholder="Password"
         value={passwordField.value}
         onChangeText={text => passwordHelpers.setValue(text)}
-        style={styles.input}
+        style={errors.password ? styles.error : styles.input}
       />
+      {errors.password ? (
+        <Text style={styles.errorText}>{errors.password}</Text>
+      ) : null}
       <Button
         mode="contained"
         style={{ backgroundColor: '#90BEDE', margin: 10 }}
@@ -51,9 +64,13 @@ const SignIn = () => {
   };
   return (
     <View style={{ width: '100%', backgroundColor: '', flex: 1, padding: 5 }}>
-      <Formik onSubmit={onSubmit} initialValues={initialValues}>
-        {({ handleSubmit }) => {
-          return <SignInForm onSubmit={handleSubmit} />;
+      <Formik
+        onSubmit={onSubmit}
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+      >
+        {({ handleSubmit, errors }) => {
+          return <SignInForm onSubmit={handleSubmit} errors={errors} />;
         }}
       </Formik>
     </View>
@@ -68,11 +85,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#F3ECE0',
     borderRadius: 10,
   },
+  inputContainer: {},
   input: {
     backgroundColor: '#F1E9DB',
     margin: 10,
+    borderBottomWidth: 3,
     borderRadius: 10,
     height: 50,
+    borderColor: '#90BEDE',
+  },
+  error: {
+    backgroundColor: '#F1E9DB',
+    margin: 10,
+    borderBottomWidth: 3,
+    borderRadius: 10,
+    height: 50,
+    borderColor: '#E3747F',
+  },
+  errorText: {
+    color: '#E3747F',
   },
 });
 
