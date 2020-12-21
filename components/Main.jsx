c55a51cfbc923c40912fba3c62e272b09981f9d1
Tanmay Gairola <tanmaygairola12@gmail.com>
2020-12-21 10:50:43 +0530

    sans serif

--- components/Main.jsx
+++ components/Main.jsx
@@ -15,6 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#DEE2EC',
     alignItems: 'center',
+    fontFamily: 'sans-serif',
     justifyContent: 'center',
   },
 });
