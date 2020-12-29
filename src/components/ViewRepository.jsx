cfe6a8bbbef0da5f83db1c0d2da4d8123f517bc1
Tanmay Gairola <tanmaygairola12@gmail.com>
2020-12-29 16:28:36 +0530

    10.19

--- src/components/ViewRepository.jsx
+++ src/components/ViewRepository.jsx
@@ -1,18 +1,81 @@
 import { useQuery } from '@apollo/client';
 import React from 'react';
-import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import {
+  Button,
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import { Card, Paragraph, Title, TouchableRipple } from 'react-native-paper';
 import { useParams } from 'react-router-native';
 import useRepository from '../hooks/useRepository';
+import * as Linking from 'expo-linking';
 
 const ViewRepository = () => {
   let { id } = useParams();
   const { repository, loading } = useRepository({ id });
+  const handleSiteOpen = () => {
+    Linking.openURL(repository.url);
+  };
   return (
     <View style={{ flex: 1 }}>
       {!loading ? (
-        <View style={styles.container}>
-          <Text>{repository.fullName}</Text>
-        </View>
+        <Card style={styles.card}>
+          <Card.Cover
+            source={{ uri: repository.ownerAvatarUrl }}
+            resizeMode="stretch"
+            style={{ height: 300, margin: 10 }}
+          />
+          <Card.Content style={{ alignItems: 'center', margin: 20 }}>
+            <Card.Title title={repository.fullName} />
+            <Paragraph>{repository.description}</Paragraph>
+            <View style={styles.language}>
+              <Text style={{ alignSelf: 'center' }}>{repository.language}</Text>
+            </View>
+            <View style={styles.info}>
+              <View style={styles.stats}>
+                <Text style={{ alignSelf: 'center', fontSize: 20 }}>
+                  {repository.stargazersCount > 1000
+                    ? `${
+                        Math.round((repository.stargazersCount / 1000) * 10) /
+                        10
+                      }k`
+                    : repository.stargazersCount}
+                </Text>
+                <Text style={{ fontSize: 20, opacity: 0.5 }}>Stars</Text>
+              </View>
+              <View style={styles.stats}>
+                <Text style={{ alignSelf: 'center', fontSize: 20 }}>
+                  {repository.forksCount > 1000
+                    ? `${Math.round((repository.forksCount / 1000) * 10) / 10}k`
+                    : repository.forksCount}
+                </Text>
+                <Text style={{ fontSize: 20, opacity: 0.5 }}>Forks</Text>
+              </View>
+
+              <View style={styles.stats}>
+                <Text style={{ alignSelf: 'center', fontSize: 20 }}>
+                  {repository.reviewCount}
+                </Text>
+                <Text style={{ fontSize: 20, opacity: 0.5 }}>Reviews</Text>
+              </View>
+
+              <View style={styles.stats}>
+                <Text style={{ alignSelf: 'center', fontSize: 20 }}>
+                  {repository.ratingAverage}
+                </Text>
+                <Text style={{ fontSize: 20, opacity: 0.5 }}>Rating</Text>
+              </View>
+            </View>
+          </Card.Content>
+          <Card.Actions>
+            <TouchableOpacity style={styles.button}>
+              <Button title="Open in GitHub" onPress={handleSiteOpen} />
+            </TouchableOpacity>
+          </Card.Actions>
+        </Card>
       ) : (
         <Text>Loading...</Text>
       )}
@@ -21,11 +84,38 @@ const ViewRepository = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    width: Dimensions.get('screen').width,
-    flex: 1,
+  card: {
+    width: Dimensions.get('screen').width - 20,
     margin: 20,
-    backgroundColor: 'orange',
+    flex: 1,
+    borderWidth: 5,
+    borderColor: '#90BEDE',
+    backgroundColor: '#F1E9DB',
+    elevation: 10,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'stretch',
+    height: '100%',
+    margin: 10,
+  },
+  language: {
+    backgroundColor: '#90BEDE',
+    width: 100,
+    alignSelf: 'center',
+    margin: 10,
+    borderRadius: 5,
+    opacity: 0.9,
+    height: 25,
+  },
+  stats: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    margin: 30,
+  },
+  info: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 });
 
