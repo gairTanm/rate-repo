97767ee1f283f580abe171aa83524bc4af4c7f95
Tanmay Gairola <tanmaygairola12@gmail.com>
2021-01-01 12:33:20 +0530

    review form

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,127 +1,169 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { useMutation } from '@apollo/react-hooks';
+import React, { useState } from 'react';
 import { Formik, useField } from 'formik';
-import { useHistory } from 'react-router-native';
+import { StyleSheet, TextInput, View, Text } from 'react-native';
 import * as yup from 'yup';
-import { CREATE_REVIEW } from '../graphql/mutations';
-import { Button, TextInput } from 'react-native-paper';
-
-const styles = StyleSheet.create({
-  textInputContainer: {
-    marginTop: 15,
-  },
-  btnContainer: {
-    marginTop: 15,
-  },
-  formContainer: {
-    padding: 15,
-  },
-  graphqlError: {
-    marginTop: 10,
-  },
-  errorText: {
-    marginTop: 5,
-  },
-});
-
-const FormikTextInput = ({ name, ...props }) => {
-  const [field, meta, helpers] = useField(name);
-  const showError = meta.touched && meta.error;
-
-  return (
-    <>
-      <TextInput
-        onChangeText={value => helpers.setValue(value)}
-        onBlur={() => helpers.setTouched(true)}
-        value={field.value}
-        error={showError}
-        {...props}
-      />
-      {showError && <Text style={styles.errorText}>{meta.error}</Text>}
-    </>
-  );
-};
-
-const CreateReviewForm = ({ createReview }) => (
-  <View>
-    <FormikTextInput name="ownerName" placeholder="Repository owner name" />
-    <View style={styles.textInputContainer}>
-      <FormikTextInput name="repositoryName" placeholder="Repository name" />
-    </View>
-    <View style={styles.textInputContainer}>
-      <FormikTextInput name="rating" placeholder="Rating between 0 and 100" />
-    </View>
-    <View style={styles.textInputContainer}>
-      <FormikTextInput name="text" placeholder="Review" multiline />
-    </View>
-    <View style={styles.btnContainer}>
-      <Button onPress={createReview}>Create a Review</Button>
-    </View>
-  </View>
-);
+import { Button, Dialog, Paragraph, Portal } from 'react-native-paper';
+import useSignUp from '../hooks/useSignUp';
+import useSignIn from '../hooks/useSignIn';
+import { useHistory } from 'react-router-native';
 
 const validationSchema = yup.object().shape({
-  ownerName: yup.string().required('Repository owner name is required'),
-  repositoryName: yup.string().required('Repository name is required'),
-  rating: yup
-    .number('Rating should be a number between 0 and 100')
-    .required('Rating is required')
-    .min(0)
-    .max(100),
-  text: yup.string(),
+  ownerName: yup.string().required('Username is required').min(3).max(30),
+  repositoryName: yup.string().required('Password is required').min(5).max(50),
+  review: yup.string().required(),
+  rating: yup.number().required(),
 });
 
 const initialValues = {
   ownerName: '',
   repositoryName: '',
-  rating: '',
-  text: '',
+  rating: null,
+  review: '',
 };
 
-const ReviewForm = () => {
-  const [mutate, { error }] = useMutation(CREATE_REVIEW);
-  const history = useHistory();
-
-  const createReview = async values => {
-    const { ownerName, repositoryName, rating, text } = values;
-    const parsedRating = +rating;
-
-    try {
-      const { data } = await mutate({
-        variables: {
-          review: {
-            ownerName,
-            repositoryName,
-            rating: parsedRating,
-            text,
-          },
-        },
-      });
-
-      if (data?.createReview) {
-        history.push(`/repository/${data?.createReview?.repositoryId}`);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
+const SignUpForm = ({ onSubmit, errors }) => {
+  const [ownerNameField, ownerNameMeta, ownerNameHelpers] = useField(
+    'ownerName'
+  );
+  const [
+    repositoryNameField,
+    repositoryNameMeta,
+    repositoryNameHelpers,
+  ] = useField('repositoryName');
+  const [ratingField, ratingMeta, ratingHelpers] = useField('rating');
+  const [reviewField, reviewMeta, reviewHelpers] = useField('review');
+  const showOwnerNameError = ownerNameMeta.touched && ownerNameMeta.error;
+  const showRepositoryNameError =
+    repositoryNameMeta.touched && repositoryNameMeta.error;
+  const showRatingError = ratingMeta.touched && ratingMeta.error;
+  const showReviewError = reviewMeta.touched && reviewMeta.error;
   return (
-    <View style={styles.formContainer}>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={createReview}
+    <View style={styles.form}>
+      <TextInput
+        placeholder="Owner Name"
+        value={ownerNameField.value}
+        style={showOwnerNameError ? styles.error : styles.input}
+        onChangeText={text => ownerNameHelpers.setValue(text)}
+      />
+      {showOwnerNameError ? (
+        <Text style={styles.errorText}>Owner name is required</Text>
+      ) : null}
+      <TextInput
+        placeholder="Repository Name"
+        value={repositoryNameField.value}
+        style={showRepositoryNameError ? styles.error : styles.input}
+        onChangeText={text => repositoryNameHelpers.setValue(text)}
+      />
+      {showRepositoryNameError ? (
+        <Text style={styles.errorText}>Repository name is required</Text>
+      ) : null}
+      <TextInput
+        placeholder="Rating"
+        value={ratingField.value}
+        style={showRatingError ? styles.error : styles.input}
+        onChangeText={text => ratingHelpers.setValue(text)}
+      />
+      {showRatingError ? (
+        <Text style={styles.errorText}>
+          Umm... we do require a rating tbh :/
+        </Text>
+      ) : null}
+
+      <TextInput
+        multiline
+        style={showReviewError ? styles.error : styles.input}
+      />
+      {showReviewError ? (
+        <Text style={styles.errorText}>What's the point if this is empty!</Text>
+      ) : null}
+      <Button
+        mode="contained"
+        testID="button"
+        style={{ backgroundColor: '#90BEDE', margin: 10 }}
+        onPress={onSubmit}
       >
-        {({ handleSubmit }) => <CreateReviewForm createReview={handleSubmit} />}
-      </Formik>
-      {error && (
-        <BodyText style={styles.graphqlError} color="error">
-          {error.message}
-        </BodyText>
-      )}
+        Submit (Hope that they'll like it)
+      </Button>
     </View>
   );
 };
+
+const ReviewForm = () => {
+  const [signUp] = useSignUp();
+  const [signIn] = useSignIn();
+  const history = useHistory();
+  const [visible, setVisible] = useState(false);
+  const onSubmit = async values => {
+    const { username, password } = values;
+    console.log({ username, password });
+  };
+
+  return (
+    <View style={{ flex: 1, width: '100%', padding: 5 }}>
+      <Portal>
+        <Dialog visible={visible}>
+          <Dialog.Title>Username Already Taken!</Dialog.Title>
+          <Dialog.Content>
+            <Text>
+              Umm... the username's already taken, try using another one.
+            </Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button
+              contained
+              style={styles.button}
+              onPress={() => setVisible(false)}
+            >
+              Umm...Okay
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        {({ handleSubmit, errors }) => (
+          <SignUpForm onSubmit={handleSubmit} errors={errors} />
+        )}
+      </Formik>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  form: {
+    width: '100%',
+    padding: 10,
+    elevation: 10,
+    backgroundColor: '#F3ECE0',
+    borderRadius: 10,
+  },
+  inputContainer: {},
+  input: {
+    backgroundColor: '#F1E9DB',
+    margin: 10,
+    borderBottomWidth: 3,
+    borderRadius: 10,
+    borderColor: '#90BEDE',
+    opacity: 0.7,
+    minHeight: 40,
+    maxHeight: 100,
+  },
+  error: {
+    backgroundColor: '#F1E9DB',
+    margin: 10,
+    opacity: 0.7,
+    borderBottomWidth: 3,
+    borderRadius: 10,
+    borderColor: '#E3747F',
+    minHeight: 40,
+    maxHeight: 100,
+  },
+  errorText: {
+    color: '#E3747F',
+  },
+});
 
 export default ReviewForm;
